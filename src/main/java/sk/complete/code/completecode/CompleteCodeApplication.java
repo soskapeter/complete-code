b40df2a5bf52d17ca8436c13f8663c82b4f5deb7
Peter Soska <soskapeter182@gmail.com>
2019-02-24 13:06:38 +0100

    initial commit

---
+++ src/main/java/sk/complete/code/completecode/CompleteCodeApplication.java
@@ -0,0 +1,13 @@
+package sk.complete.code.completecode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompleteCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompleteCodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sk/complete/code/completecode/CompleteCodeApplicationTests.java
@@ -0,0 +1,16 @@
+package sk.complete.code.completecode;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CompleteCodeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
